d9127927410e67ac709cff8483693eb098cdad50
nildo-santos <nil-do-fox@hotmail.com>
2025-11-25 21:12:29 -0300

    Versionamento da API

--- Clientes.API/Controllers/ClientesController.cs
+++ Clientes.API/Controllers/ClientesController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Clientes.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
     {
